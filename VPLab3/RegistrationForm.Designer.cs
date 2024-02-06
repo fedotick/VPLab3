0740f0138c47fbb9b56944d72f82a08abd306227
fedotick <feodortcaciuc2004@gmail.com>
2024-02-06 05:37:08 +0200

    Stylized registration form

--- VPLab3/RegistrationForm.Designer.cs
+++ VPLab3/RegistrationForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistrationForm));
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
@@ -42,33 +43,45 @@
             this.btnConfirm = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.linkLabelLogin = new System.Windows.Forms.LinkLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtUsername
             // 
             this.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtUsername.Location = new System.Drawing.Point(299, 71);
+            this.txtUsername.Location = new System.Drawing.Point(27, 52);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(156, 20);
             this.txtUsername.TabIndex = 0;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(299, 97);
+            this.txtPassword.Location = new System.Drawing.Point(27, 91);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(156, 20);
             this.txtPassword.TabIndex = 0;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(299, 123);
+            this.txtEmail.Location = new System.Drawing.Point(27, 130);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(156, 20);
             this.txtEmail.TabIndex = 0;
             // 
             // txtFullName
             // 
-            this.txtFullName.Location = new System.Drawing.Point(299, 149);
+            this.txtFullName.Location = new System.Drawing.Point(27, 169);
             this.txtFullName.Name = "txtFullName";
             this.txtFullName.Size = new System.Drawing.Size(156, 20);
             this.txtFullName.TabIndex = 0;
@@ -77,7 +90,7 @@
             // 
             this.radioButtonF.AutoSize = true;
             this.radioButtonF.Checked = true;
-            this.radioButtonF.Location = new System.Drawing.Point(299, 176);
+            this.radioButtonF.Location = new System.Drawing.Point(44, 19);
             this.radioButtonF.Name = "radioButtonF";
             this.radioButtonF.Size = new System.Drawing.Size(31, 17);
             this.radioButtonF.TabIndex = 1;
@@ -88,7 +101,7 @@
             // radioButtonM
             // 
             this.radioButtonM.AutoSize = true;
-            this.radioButtonM.Location = new System.Drawing.Point(336, 175);
+            this.radioButtonM.Location = new System.Drawing.Point(81, 19);
             this.radioButtonM.Name = "radioButtonM";
             this.radioButtonM.Size = new System.Drawing.Size(34, 17);
             this.radioButtonM.TabIndex = 1;
@@ -98,14 +111,14 @@
             // dateOfBirth
             // 
             this.dateOfBirth.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.dateOfBirth.Location = new System.Drawing.Point(299, 198);
+            this.dateOfBirth.Location = new System.Drawing.Point(27, 264);
             this.dateOfBirth.Name = "dateOfBirth";
             this.dateOfBirth.Size = new System.Drawing.Size(156, 20);
             this.dateOfBirth.TabIndex = 2;
             // 
             // txtNativeLanguage
             // 
-            this.txtNativeLanguage.Location = new System.Drawing.Point(299, 224);
+            this.txtNativeLanguage.Location = new System.Drawing.Point(27, 303);
             this.txtNativeLanguage.Name = "txtNativeLanguage";
             this.txtNativeLanguage.Size = new System.Drawing.Size(156, 20);
             this.txtNativeLanguage.TabIndex = 3;
@@ -113,7 +126,7 @@
             // checkBoxRo
             // 
             this.checkBoxRo.AutoSize = true;
-            this.checkBoxRo.Location = new System.Drawing.Point(299, 250);
+            this.checkBoxRo.Location = new System.Drawing.Point(20, 20);
             this.checkBoxRo.Name = "checkBoxRo";
             this.checkBoxRo.Size = new System.Drawing.Size(74, 17);
             this.checkBoxRo.TabIndex = 4;
@@ -123,7 +136,7 @@
             // checkBoxEn
             // 
             this.checkBoxEn.AutoSize = true;
-            this.checkBoxEn.Location = new System.Drawing.Point(299, 273);
+            this.checkBoxEn.Location = new System.Drawing.Point(20, 43);
             this.checkBoxEn.Name = "checkBoxEn";
             this.checkBoxEn.Size = new System.Drawing.Size(60, 17);
             this.checkBoxEn.TabIndex = 4;
@@ -133,7 +146,7 @@
             // checkBoxDe
             // 
             this.checkBoxDe.AutoSize = true;
-            this.checkBoxDe.Location = new System.Drawing.Point(299, 296);
+            this.checkBoxDe.Location = new System.Drawing.Point(20, 66);
             this.checkBoxDe.Name = "checkBoxDe";
             this.checkBoxDe.Size = new System.Drawing.Size(63, 17);
             this.checkBoxDe.TabIndex = 4;
@@ -142,7 +155,7 @@
             // 
             // btnConfirm
             // 
-            this.btnConfirm.Location = new System.Drawing.Point(299, 319);
+            this.btnConfirm.Location = new System.Drawing.Point(27, 428);
             this.btnConfirm.Name = "btnConfirm";
             this.btnConfirm.Size = new System.Drawing.Size(75, 23);
             this.btnConfirm.TabIndex = 5;
@@ -152,7 +165,7 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(380, 319);
+            this.btnClear.Location = new System.Drawing.Point(108, 428);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 23);
             this.btnClear.TabIndex = 5;
@@ -163,7 +176,8 @@
             // linkLabelLogin
             // 
             this.linkLabelLogin.AutoSize = true;
-            this.linkLabelLogin.Location = new System.Drawing.Point(357, 345);
+            this.linkLabelLogin.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.linkLabelLogin.Location = new System.Drawing.Point(89, 480);
             this.linkLabelLogin.Name = "linkLabelLogin";
             this.linkLabelLogin.Size = new System.Drawing.Size(33, 13);
             this.linkLabelLogin.TabIndex = 6;
@@ -171,30 +185,133 @@
             this.linkLabelLogin.Text = "Login";
             this.linkLabelLogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelLogin_LinkClicked);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.linkLabelLogin);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.btnConfirm);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNativeLanguage);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.dateOfBirth);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtFullName);
+            this.groupBox1.Location = new System.Drawing.Point(20, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 520);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Register";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(24, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(25, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "E-mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(24, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Full name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonM);
+            this.groupBox2.Controls.Add(this.radioButtonF);
+            this.groupBox2.Location = new System.Drawing.Point(27, 195);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(156, 50);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(25, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Date of birth";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(25, 287);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Native language";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBoxDe);
+            this.groupBox3.Controls.Add(this.checkBoxRo);
+            this.groupBox3.Controls.Add(this.checkBoxEn);
+            this.groupBox3.Location = new System.Drawing.Point(27, 329);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(156, 93);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Foreign languages";
+            // 
             // RegistrationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.linkLabelLogin);
-            this.Controls.Add(this.btnClear);
-            this.Controls.Add(this.btnConfirm);
-            this.Controls.Add(this.checkBoxDe);
-            this.Controls.Add(this.checkBoxEn);
-            this.Controls.Add(this.checkBoxRo);
-            this.Controls.Add(this.txtNativeLanguage);
-            this.Controls.Add(this.dateOfBirth);
-            this.Controls.Add(this.radioButtonM);
-            this.Controls.Add(this.radioButtonF);
-            this.Controls.Add(this.txtFullName);
-            this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.txtUsername);
+            this.ClientSize = new System.Drawing.Size(264, 561);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "RegistrationForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Registration";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -214,5 +331,14 @@
         private System.Windows.Forms.Button btnConfirm;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.LinkLabel linkLabelLogin;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
     }
 }
